504f486e93fadb296aa6aedbad2dc5fc8f9c7b31
Ronil Rufo <ronil.rufo@gmail.com>
2021-11-27 12:48:38 +0800

    Added factories for tests

---
+++ listings/tests/factories.py
@@ -0,0 +1,47 @@
+import factory
+
+
+class ListingFactory(factory.django.DjangoModelFactory):
+    """
+    Factory for :model:`listings.Listing`
+    """
+
+    class Meta:
+        model = "listings.Listing"
+
+
+class HotelRoomTypeFactory(factory.django.DjangoModelFactory):
+    """
+    Factory for :model:`listings.HotelRoomType`
+    """
+
+    class Meta:
+        model = "listings.HotelRoomType"
+
+
+class HotelRoomFactory(factory.django.DjangoModelFactory):
+    """
+    Factory for :model:`listings.HotelRoom`
+    """
+
+    class Meta:
+        model = "listings.HotelRoom"
+
+
+class BookingInfoFactory(factory.django.DjangoModelFactory):
+    """
+    Factory for :model:`listings.BookingInfo`
+    """
+
+    class Meta:
+        model = "listings.BookingInfo"
+        django_get_or_create = ("listing", "hotel_room_type")
+
+
+class BookingReservationFactory(factory.django.DjangoModelFactory):
+    """
+    Factory for :model:`listings.BookingReservation`
+    """
+
+    class Meta:
+        model = "listings.BookingReservation"
