4d0171105f1cf31c72a31fd583be6f1e40125d0b
Ronil Rufo <ronil.rufo@gmail.com>
2021-11-27 22:04:52 +0800

    Added tests for room availability validation in BookingReservationSerializer

--- listings/tests/test_booking_reservation.py
+++ listings/tests/test_booking_reservation.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from ..models import Listing
 from .mixins import ListingsTestMixin
 
 
@@ -14,11 +15,13 @@ class BookingReservationTests(ListingsTestMixin, APITestCase):
     Test cases for `reservations` endpoints(:views:`listings.BookingReservationViewSet`)
     """
 
-    def test_create_booking_reservation(self):
+    def test_create_booking_reservation_apartment(self):
         """
-        Test successful creation of a booking reservation.
+        Test successful creation of a booking reservation of an apartment.
         """
-        booking_info = self.create_booking_info()
+        booking_info = self.create_booking_info(
+            listing=self.create_listing(listing_type=Listing.APARTMENT)
+        )
         start_date = (timezone.now() + relativedelta(days=3)).date()
         end_date = start_date + relativedelta(days=2)
         payload = {
@@ -30,6 +33,54 @@ class BookingReservationTests(ListingsTestMixin, APITestCase):
         url = reverse("reservations-list")
         response = self.client.post(url, payload)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_booking_reservation_hotel(self):
+        """
+        Test successful creation of a booking reservation of a hotel.
+        """
+        booking_info = self.create_booking_info(
+            hotel_room_type=self.create_hotel_room_type()
+        )
+        self.create_hotel_room(hotel_room_type=booking_info.hotel_room_type)
+        start_date = (timezone.now() + relativedelta(days=3)).date()
+        end_date = start_date + relativedelta(days=2)
+        payload = {
+            "booking_info": booking_info.id,
+            "start_date": start_date.strftime("%Y-%m-%d"),
+            "end_date": end_date.strftime("%Y-%m-%d"),
+        }
+
+        url = reverse("reservations-list")
+        response = self.client.post(url, payload)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_booking_reservation_insufficient_rooms(self):
+        """
+        Test raising ValidationError when creating a reservation when rooms are fully
+        booked for the specified date duration.
+        """
+        booking_info = self.create_booking_info(
+            hotel_room_type=self.create_hotel_room_type()
+        )
+        self.create_hotel_room(hotel_room_type=booking_info.hotel_room_type)
+        start_date = (timezone.now() + relativedelta(days=3)).date()
+        end_date = start_date + relativedelta(days=2)
+
+        # Create overlapping reservation
+        self.create_booking_reservation(
+            booking_info=booking_info,
+            start_date=start_date + relativedelta(days=1),
+            end_date=end_date + relativedelta(days=2),
+        )
+        payload = {
+            "booking_info": booking_info.id,
+            "start_date": start_date.strftime("%Y-%m-%d"),
+            "end_date": end_date.strftime("%Y-%m-%d"),
+        }
+
+        url = reverse("reservations-list")
+        response = self.client.post(url, payload)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_booking_reservation_invalid_date_range(self):
         """
